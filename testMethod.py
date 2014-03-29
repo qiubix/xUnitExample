1ea1d2236e403dbeec2d399f67499d93fd3faccf
qiubix <qiubix@gmail.com>
2014-03-29 17:47:41 +0100

    First test method - refactoring

--- testMethod.py
+++ testMethod.py
@@ -1,11 +1,19 @@
-class WasRun:
+class TestCase:
+	def __init__(self, name):
+		self.name = name
+	def run(self):
+		method = getattr(self, self.name)
+		method()
+
+class WasRun(TestCase):
 	def __init__(self, name):
 		self.wasRun= None
+		TestCase.__init__(self, name)
 	def testMethod(self):
 		self.wasRun= 1
 
 test= WasRun("testMethod")
 print test.wasRun
-test.testMethod()
+test.run()
 print test.wasRun
 
