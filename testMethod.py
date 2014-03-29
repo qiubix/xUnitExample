017b3dfb0ef28ff0e67ddb97b98a476b14253c76
qiubix <qiubix@gmail.com>
2014-03-29 17:25:54 +0100

    First test method

---
+++ testMethod.py
@@ -0,0 +1,11 @@
+class WasRun:
+	def __init__(self, name):
+		self.wasRun= None
+	def testMethod(self):
+		self.wasRun= 1
+
+test= WasRun("testMethod")
+print test.wasRun
+test.testMethod()
+print test.wasRun
+
