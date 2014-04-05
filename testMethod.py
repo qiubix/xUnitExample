f1fd3e87c54d65e5d4048c0706b035f27d325812
qiubix <qiubix@gmail.com>
2014-04-06 00:06:07 +0200

    Add setUp test

--- testMethod.py
+++ testMethod.py
@@ -1,7 +1,10 @@
 class TestCase:
 	def __init__(self, name):
 		self.name = name
+	def setUp(self):
+		pass
 	def run(self):
+		self.setUp()
 		method = getattr(self, self.name)
 		method()
 
@@ -11,6 +14,8 @@ class WasRun(TestCase):
 		TestCase.__init__(self, name)
 	def testMethod(self):
 		self.wasRun= 1
+	def setUp(self):
+		self.wasSetUp= 1
 
 class TestCaseTest(TestCase):
 	def testRunning(self):
@@ -18,5 +23,9 @@ class TestCaseTest(TestCase):
 		assert(not test.wasRun)
 		test.run()
 		assert(test.wasRun)
+	def testSetUp(self):
+		test = WasRun("testMethod")
+		test.run()
+		assert(test.wasSetUp)
 
-TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
