360b13577e23251f3847fb6b1035c6c36a4d8195
qiubix <qiubix@gmail.com>
2014-04-06 14:05:55 +0200

    Add testSuite test, implement TestSuite class

--- testMethod.py
+++ testMethod.py
@@ -45,6 +45,17 @@ class TestResult:
 	def summary(self):
 		return "%d run, %d failed" % (self.runCount, self.errorCount)
 
+class TestSuite:
+	def __init__(self):
+		self.tests = []
+	def add(self, test):
+		self.tests.append(test)
+	def run(self):
+		result = TestResult()
+		for test in self.tests:
+			test.run(result)
+		return result
+
 class TestCaseTest(TestCase):
 	def testTemplateMethod(self):
 		test = WasRun("testMethod")
@@ -63,8 +74,15 @@ class TestCaseTest(TestCase):
 		test = WasRun("testBrokenMethod")
 		result = test.run()
 		assert("1 run, 1 failed" == result.summary)
+	def testSuite(self):
+		suite = TestSuite()
+		suite.add(WasRun("testMethod"))
+		suite.add(WasRun("testBrokenMethod"))
+		result = suite.run()
+		assert("2 run, 1 failed" == result.summary())
 
 print TestCaseTest("testTemplateMethod").run().summary()
 print TestCaseTest("testResult").run().summary()
 print TestCaseTest("testFailedResultFormatting").run().summary()
 print TestCaseTest("testFailedResult").run().summary()
+print TestCaseTest("testSuite").run().summary()
