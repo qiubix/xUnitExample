e08349ca395c21bd9016b6e4be13f16d1f276fc2
qiubix <qiubix@gmail.com>
2014-04-06 12:56:19 +0200

    Remove warRun flag.

--- testMethod.py
+++ testMethod.py
@@ -15,10 +15,8 @@ class WasRun(TestCase):
 	def __init__(self, name):
 		TestCase.__init__(self, name)
 	def testMethod(self):
-		self.wasRun= 1
 		self.log = self.log + "testMethod "
 	def setUp(self):
-		self.wasRun= None
 		self.log = "setUp "
 	def tearDown(self):
 		self.log = self.log + "tearDown "
