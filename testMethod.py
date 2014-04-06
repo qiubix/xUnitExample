f1e4dc7bd2be1fa558adc206a5b589cadf1f2320
qiubix <qiubix@gmail.com>
2014-04-06 13:32:23 +0200

    Print test results

--- testMethod.py
+++ testMethod.py
@@ -64,7 +64,7 @@ class TestCaseTest(TestCase):
 		result = test.run()
 		assert("1 run, 1 failed" == result.summary)
 
-TestCaseTest("testTemplateMethod").run()
-TestCaseTest("testResult").run()
-TestCaseTest("testFailedResultFormatting").run()
-TestCaseTest("testFailedResult").run()
+print TestCaseTest("testTemplateMethod").run().summary()
+print TestCaseTest("testResult").run().summary()
+print TestCaseTest("testFailedResultFormatting").run().summary()
+print TestCaseTest("testFailedResult").run().summary()
