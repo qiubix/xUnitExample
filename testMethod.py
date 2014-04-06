e3a5b2b5abb3619893e680485ceee1d1accbfddc
qiubix <qiubix@gmail.com>
2014-04-06 13:09:13 +0200

    Report collected results.

--- testMethod.py
+++ testMethod.py
@@ -6,10 +6,13 @@ class TestCase:
 	def tearDown(self):
 		pass
 	def run(self):
+		result = TestResult()
+		result.testStarted()
 		self.setUp()
 		method = getattr(self, self.name)
 		method()
 		self.tearDown()
+		return result
 
 class WasRun(TestCase):
 	def __init__(self, name):
@@ -21,10 +24,23 @@ class WasRun(TestCase):
 	def tearDown(self):
 		self.log = self.log + "tearDown "
 
+class TestResult:
+	def __init__(self):
+		self.runCount = 0
+	def testStarted(self):
+		self.runCount = self.runCount + 1
+	def summary(self):
+		return "%d run, 0 failed" % self.runCount
+
 class TestCaseTest(TestCase):
 	def testTemplateMethod(self):
 		test = WasRun("testMethod")
 		test.run()
 		assert("setUp testMethod tearDown " == test.log)
+	def testResult(self):
+		test = WasRun("testMethod")
+		result = test.run()
+		assert("1 run, 0 failed" == result.summary())
 
 TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
